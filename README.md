b6f6700a0fb27d43117a55a47e8238f162c7d104
mikhaild908 <mikedumlao@Mikes-MacBook-Pro.local>
2025-01-06 22:17:03 -0700

    update readme; update main.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PencilSketch
+```
+1. python3 -m venv venv
+2. source venv/bin/activate
+3. which python
+4. python --version
+5. pip install --upgrade pip
+6. pip list
+7. pip install -r requirements.txt
+8. deactivate
+```
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import cv2
-# image = cv2.imread("sonic-01.png")
-image = cv2.imread("lauren.jpg")
+image = cv2.imread("sonic-01.png")
+# image = cv2.imread("lauren.jpg")
 grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 inverted_image = cv2.bitwise_not(grey_image)
 blurred_image = cv2.GaussianBlur(inverted_image, (21, 21), 0)
 inverted_blurred_image = cv2.bitwise_not(blurred_image)
 sketch = cv2.divide(grey_image, inverted_blurred_image, scale=256.0)
 
-# cv2.imwrite("sketch-01.png", sketch)
-cv2.imwrite("lauren-sketch-01.png", sketch)
+cv2.imwrite("sketch-01.png", sketch)
+# cv2.imwrite("lauren-sketch-01.png", sketch)
